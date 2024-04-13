673bab3b0c4e07e238b605db96b934ecc5ac96d0
Kyle Grierson <griersonky@gmail.com>
2024-04-13 06:59:01 +0100

    reitit name based routing for HATEOAS links

--- src/template/resource.clj
+++ src/template/resource.clj
@@ -9,53 +9,67 @@
    [reitit.ring.middleware.parameters :as parameters]
    [template.aggregate :as aggregate]
    [template.events :as events]
-   [template.projection :as projection]))
+   [template.projection :as projection]
+   [reitit.core :as r]))
 
 (require 'hashp.core)
 
-(defn app
-  [{:keys [event-store]}]
-  (ring/ring-handler
-   (ring/router
-    [["/health"
-      {:get
-       {:handler (fn [_]
+(defn make-routes [{:keys [event-store]}]
+  [["" {:name ::discovery
+        :get {:handler (fn [{::r/keys [router]}]
+                         {:status 200
+                          :body {:links {:self
+                                         (-> router
+                                             (r/match-by-name ::discovery)
+                                             (r/match->path))}}})}}]
+   ["/health"
+    {:name ::health
+     :get
+     {:handler (fn [_] {:status 200 :body "healthy"})}}]
+
+   ["/events"
+    {:name ::events
+     :get
+     {:parameters {:query {:start (l/optional int?)
+                           :end  (l/optional int?)}}
+      :handler (fn [{{{:keys [start end]
+                       :or {start 0
+                            end (+ start 10)}} :query} :parameters}]
+                 {:status 200
+                  :body (events/get-events event-store start end)})}}]
+   ["/aggregate/:id"
+    {:name ::aggregate
+     :get
+     {:parameters {:path {:id uuid?}}
+      :handler (fn [{{{:keys [id]} :path} :parameters}]
+                 (let [aggregate (projection/get-by-id event-store id)]
                    {:status 200
-                    :body "healthy"})}}]
-     ["/events"
-      {:get
-       {:parameters {:query {:start (l/optional int?)
-                             :end  (l/optional int?)}}
-        :handler (fn [{{{:keys [start end]
-                         :or {start 0
-                              end (+ start 10)}} :query} :parameters}]
-                   {:status 200
-                    :body (events/get-events event-store start end)})}}]
-     ["/aggregate/:id"
-      {:get
-       {:parameters {:path {:id uuid?}}
-        :handler (fn [{{{:keys [id]} :path} :parameters}]
-                   (let [aggregate (projection/get-by-id event-store id)]
-                     {:status 200
-                      :body aggregate}))}}]
-     ["/aggregates"
-      {:get
-       {:handler (fn [_]
-                   {:status 200
-                    :body (projection/get-projections event-store)})}
-       :post
-       {:parameters {:body [:map [:name string?]]}
-        :handler (fn [{{{:keys [name]} :body} :parameters}]
-                   (let [aggregate (aggregate/create-aggregate event-store {:name name})]
-                     {:status 201
-                      :body (merge
-                             aggregate
-                             {:links {:self (str "/aggregate/" (:id aggregate))}})}))}}]]
-    {:data       {:coercion mcoercion/coercion
-                  :muuntaja   m/instance
-                  :middleware [parameters/parameters-middleware
-                               muuntaja/format-negotiate-middleware
-                               muuntaja/format-request-middleware
-                               muuntaja/format-response-middleware
-                               rrc/coerce-request-middleware
-                               rrc/coerce-response-middleware]}})))
+                    :body aggregate}))}}]
+   ["/aggregates"
+    {:name ::aggregates
+     :get
+     {:handler (fn [_]
+                 {:status 200
+                  :body (projection/get-projections event-store)})}
+     :post
+     {:parameters {:body [:map [:name string?]]}
+      :handler (fn [{{{:keys [name]} :body} :parameters}]
+                 (let [aggregate (aggregate/create-aggregate event-store {:name name})]
+                   {:status 201
+                    :body (merge
+                           aggregate
+                           {:links {:self (str "/aggregate/" (:id aggregate))}})}))}}]])
+
+(defn make-router [dependencies]
+  (ring/router
+   (make-routes dependencies)
+   {:data       {:coercion mcoercion/coercion
+                 :muuntaja   m/instance
+                 :middleware [parameters/parameters-middleware
+                              muuntaja/format-negotiate-middleware
+                              muuntaja/format-request-middleware
+                              muuntaja/format-response-middleware
+                              rrc/coerce-request-middleware
+                              rrc/coerce-response-middleware]}}))
+
+(defn app [dependencies] (ring/ring-handler (make-router dependencies)))
--- test/template/resource_test.clj
+++ test/template/resource_test.clj
@@ -13,19 +13,28 @@
   {:handler (get-in system [::ds/instances :http :handler])
    :event-store (get-in system [::ds/instances :components :event-store])})
 
-(defn request->map [request]
+(defn request->resource [request]
   (-> request
       :body
       slurp
       (json/read-value json/keyword-keys-object-mapper)))
+
+(deftest discovery-test
+  (let [{:keys [handler]} (extract ds/*system*)
+        request (handler {:request-method :get
+                          :uri ""})
+        resource (request->resource request)]
+    (testing "returns 200"
+      (is (= 200 (:status request))))
+    (testing "contains discovery link"
+      (is (= {:links {:self ""}} resource)))))
 
 (deftest health-test
   (let [{:keys [handler]} (extract ds/*system*)
         request (handler {:request-method :get
                           :uri "/health"})]
     (testing "Calling health returns 200"
-      (is (= {:status 200
-              :body  "healthy"}
+      (is (= {:status 200 :body "healthy"}
              request)))))
 
 (deftest events-test
@@ -36,26 +45,29 @@
       (testing "Calling events returns 200"
         (is (= 200 (:status request))))
       (testing "Contains events"
-        (is (= 0 (count (request->map request))))))))
+        (is (= 0 (count (request->resource request))))))))
 
 (deftest events-found-test
   (let [{:keys [handler]} (extract ds/*system*)
         name "alice"
-        created-aggregregate
+
+        post-aggregregates-request
         (handler {:request-method :post
                   :uri "/aggregates"
                   :body-params {:name name}})
-        created-resource (request->map created-aggregregate)
-        request (handler {:request-method :get
-                          :uri "/events"})
-        events (request->map request)
-        event (first events)]
+        post-aggregates-resource
+        (request->resource post-aggregregates-request)
+
+        events-request (handler {:request-method :get
+                                 :uri "/events"})
+        events-resource (request->resource events-request)
+        event (first events-resource)]
     (testing "/events"
       (testing "Calling events returns 200"
-        (is (= 200 (:status request))))
+        (is (= 200 (:status events-request))))
       (testing "Contains events"
-        (is (= 1 (count events)))
-        (is (= {:name name :id (:id created-resource)}
+        (is (= 1 (count events-resource)))
+        (is (= {:name name :id (:id post-aggregates-resource)}
                (:events/data event)))
         (is (= "aggregate-created" (:events/type event)))))))
 
@@ -65,29 +77,34 @@
         request (handler {:request-method :post
                           :uri "/aggregates"
                           :body-params {:name name}})
-        response (request->map request)]
+        resource (request->resource request)]
     (testing "/aggregates"
       (testing "Calling POST returns 201"
         (is (= 201 (:status request))))
       (testing "Contains properties"
-        (is (= name (:name response)))))))
+        (is (= name (:name resource)))))))
 
 (deftest get-aggregate-test
   (let [{:keys [handler]} (extract ds/*system*)
         name "alice"
-        created-aggregate
+
+        post-aggregates-request
         (handler {:request-method :post
                   :uri "/aggregates"
                   :body-params {:name name}})
-        data (request->map created-aggregate)
-        request (handler {:request-method :get
-                          :uri (get-in data [:links :self])})
-        response (request->map request)]
+        post-aggregates-resource
+        (request->resource post-aggregates-request)
+
+        request
+        (handler {:request-method :get
+                  :uri (get-in post-aggregates-resource [:links :self])})
+
+        resource (request->resource request)]
     (testing "/aggregate/:id"
       (testing "Calling GET returns 200"
         (is (= 200 (:status request))))
       (testing "Contains properties"
-        (is (= name (get-in response [:projections/data :name])))))))
+        (is (= name (get-in resource [:projections/data :name])))))))
 
 (deftest get-aggregates-test
   (let [{:keys [handler]} (extract ds/*system*)
@@ -100,9 +117,9 @@
                     :body-params {:name name2}})
         request (handler {:request-method :get
                           :uri "/aggregates"})
-        response (request->map request)]
+        resource (request->resource request)]
     (testing "GET /aggregates"
       (testing "returns 200"
         (is (= 200 (:status request))))
       (testing "properties"
-        (is (= 2 (count response)))))))
+        (is (= 2 (count resource)))))))
