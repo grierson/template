293982b7d119e10432c76ad3f737b0810c741642
Kyle Grierson <griersonky@gmail.com>
2024-04-19 20:48:26 +0100

    docker context use default :)

--- bb/testdatabase.clj
+++ bb/testdatabase.clj
@@ -3,4 +3,10 @@
 (ns testdatabase
   (:require [contajners.core :as c]))
 
-(c/categories :docker "v1.41")
+(def images-docker
+  (c/client {:engine   :docker
+             :category :images
+             :version  "v1.41"
+             :conn     {:uri "unix:///var/run/docker.sock"}}))
+
+(c/invoke images-docker {:op :ImageList})
